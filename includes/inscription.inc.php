117e173e243f44c637d46c69c0d1ba03b47a7459
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 16:02:11 +0100

    Utilisation de la classe User pour s'inscrire

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,65 +1,56 @@
 <h1>Inscription</h1>
 <?php
-$nom = $prenom = $mail = '';
 
-$formCreator = new Form("post", "index.php?page=inscription");
-$formCreator->getFormValues();
-$form = $formCreator->createFormFromCSV("./assets/frmFiles/inscription.csv");
-
-if (isset($_POST['envoi'])) 
+if (isset($_SESSION['login']))
 {
+    echo "<script>
+    document.location.replace('http://localhost/GEIPAN/index.php?page=404');
+    </script>";
+}
 
-    $errorHand = new ErrorHandler;
+else
+{
+    $formCreator = new Form("post", "index.php?page=inscription");
+    $formCreator->getFormValues();
+    $form = $formCreator->createFormFromCSV("./assets/frmFiles/inscription.csv");
 
-    $formCreator->setValuesChecker($errorHand);
-
-    $formCreator->checkValues();
-
-    if ($formCreator->getErrorsCount() === 0) 
+    if (isset($_POST['envoi'])) 
     {
-        $connHand = new Query;
 
-        $resultat = $connHand->select("SELECT * FROM users WHERE usermail='". $formCreator->getValue("mail") . "'");
-        
-        if (count($resultat) !== 0) 
+        $errorHand = new ErrorHandler;
+
+        $formCreator->setValuesChecker($errorHand);
+
+        $formCreator->checkValues();
+
+
+        if ($formCreator->getErrorsCount() === 0) 
         {
-            echo "<p>Votre adresse est déjà enregistrée dans la base de données</p>";
-        }
+
+
+            $connHand = new Query;
+
+            $user = new Registered();
+            $user->setNom($formCreator->getValue("nom"));
+            $user->setPrenom($formCreator->getValue("prenom"));
+            $user->setMail($formCreator->getValue("mail"));
+            $user->setAvatar("dummy" /* $formCreator->getValue("Avatar") */);
+            $user->setConnexion($connHand);
+
+            $user->inscription($formCreator->getValue("mdp"));
+        } 
 
         else 
         {
-            $token = sha1(getRandomString(128));
-
-            $connHand->insertion("
-            INSERT INTO users(userName, userFirstname, userMail, userPassword, id_role, usertoken)
-            VALUES ('". $formCreator->getValue("nom") 
-            . "', '". $formCreator->getValue("prenom") 
-            . "', '". $formCreator->getValue("mail") 
-            . "', '". password_hash($formCreator->getValue("mdp"), PASSWORD_DEFAULT) . "', 2, '$token')
-            ");
-            
-            $from = "contact.news@gmail.com";
-            $to = $formCreator->getValue("mail");
-            $subject = "Vérification de votre compte";
-            $header = "Content-type: text/html; charset=iso-8859-1\nFrom:" . $from;
-            $message = "<a href='http://localhost/GEIBAN/index.php?page=mailValidation&token=" 
-            . urlencode($token) . "&mail=" 
-                . urlencode($formCreator->getValue("mail")) . "' target='_blank'>Cliquez sur ce lien pour valider votre compte</a>";
-            mail($to, $subject, $message, $header);
+            echo $formCreator->checkErrors();
+            echo $form;
         }
+
     } 
 
     else 
     {
-        echo $formCreator->checkErrors();
+        echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
         echo $form;
     }
-
-} 
-
-else 
-{
-    echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
-    echo $form;
 }
-
