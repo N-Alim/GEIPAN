9b1cbabd528d3636d2ceb9e6f959f7add273313e
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 10:27:56 +0100

    Ajout de pages dans la navigation

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,16 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
+            <?php
+                if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
+                    echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                    echo "<li><a href=\"index.php?page=myaccount\">Mon compte</a></li>";
+                }
+                else {
+                    echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+                    echo "<li><a href=\"index.php?page=inscription\">Inscription</a></li>";
+                }
+            ?>
         </ul>
     </nav>
 </header>
