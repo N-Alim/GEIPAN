6e08e9df19b846e0065c5480857d079157ed1a84
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 11:39:16 +0100

    Ajout de champ d'entrée possibles

--- classes/InputType.php
+++ classes/InputType.php
@@ -4,12 +4,15 @@ enum InputType
 {
     case Text;
     case File;
+    case FileMultiple;
     case LongText;
     case Mail;
     case Password;
     case PasswordInit;
     case PasswordVerif;
     case Date;
+    case DateTime;
     case Select;
+    case SelectMultiple
 }
 
