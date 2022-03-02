a014e8ab3076aa942eb642756d7857bd21357feb
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 10:23:43 +0100

    Ajout page de connexion

---
+++ includes/login.inc.php
@@ -0,0 +1,73 @@
+<h1>Login</h1>
+<?php
+
+
+
+$formCreator = new Form("post", "index.php?page=login");
+$formCreator->getFormValues();
+$form = $formCreator->createForm([
+    "mail" => [InputType::Mail, false, "Mail"],
+    "mdp" => [InputType::Password, false, "Mot de passe"]
+]);
+
+if (isset($_POST['envoi']))
+{
+
+    $errorHand = new ErrorHandler;
+
+    $formCreator->setValuesChecker($errorHand);
+
+    $formCreator->checkValues();
+
+    if ($formCreator->getErrorsCount() === 0)
+    {
+        $connHand = new Sql;
+
+        $resultat = $connHand->select("SELECT * FROM t_users WHERE usermail='" . $formCreator->getValue("mail") . "'");
+
+        if (count($resultat) === 0)
+        {
+            echo "Pas de résultat avec votre login/mot de passe";
+        }
+
+        else
+        {
+            $mdpRequete = $resultat[0]->USEPASSWORD;
+            if (password_verify($formCreator->getValue("mdp"), $mdpRequete))
+            {
+                if (!isset($_SESSION['login']))
+                {
+                    $_SESSION['login'] = true;
+                    $_SESSION['nom'] = $resultat[0]->USENAME;
+                    $_SESSION['prenom'] = $resultat[0]->USEFIRSTNAME;
+                    $_SESSION['role'] = $resultat[0]->ID_ROLE;
+                    echo "<script>
+                    document.location.replace('http://localhost/ExercicePHP/');
+                    </script>";
+                }
+
+                else
+                {
+                    echo "Vous êtes déjà connecté, donc vous n'avez rien à faire ici";
+                }
+            }
+
+            else
+            {
+                echo "Bien tenté, mais non";
+            }
+        }
+    }
+
+    else
+    {
+        echo $formCreator->checkErrors();
+        echo $form;
+    }
+}
+
+else
+{
+    echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
+    echo $form;
+}
