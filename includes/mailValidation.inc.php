5e456e4cad532a449992686c342f3310d0882b49
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 11:55:41 +0100

    Correction url envoyé dans le mail de validation

--- includes/mailValidation.inc.php
+++ includes/mailValidation.inc.php
@@ -2,10 +2,7 @@
 
 dump($_GET);
 dump(urldecode($_GET["mail"]));
-dump(urldecode( password_verify("hi@gmail.com", $_GET["mail"])));
 
 
 echo "<h1>Votre compte a bien été validé</h1>";
-echo "<h2>Redirection en cours</h2>";
-
-echo "<script>setTimeout(document.location.replace, 1000, 'index.php?page=login');</script>";
+echo "<h2>Vous pouvez maintenant vous connectez</h2>";
