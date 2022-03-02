184b5421e3ebcb96fb14f84349859812a0ab2e32
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 10:23:32 +0100

    Ajout page d'inscription

---
+++ includes/inscription.inc.php
@@ -0,0 +1,65 @@
+<h1>Inscription</h1>
+<?php
+$nom = $prenom = $mail = '';
+
+$formCreator = new Form("post", "index.php?page=inscription");
+$formCreator->getFormValues();
+$form = $formCreator->createFormFromCSV("./assets/frmFiles/inscription.csv");
+
+if (isset($_POST['envoi'])) 
+{
+
+    $errorHand = new ErrorHandler;
+
+    $formCreator->setValuesChecker($errorHand);
+
+    $formCreator->checkValues();
+
+    if ($formCreator->getErrorsCount() === 0) 
+    {
+        $connHand = new Query;
+
+        $resultat = $connHand->select("SELECT * FROM t_users WHERE usermail='". $formCreator->getValue("mail") . "'");
+        
+        if (count($resultat) !== 0) 
+        {
+            echo "<p>Votre adresse est déjà enregistrée dans la base de données</p>";
+        }
+
+        else 
+        {
+            $token = sha1(getRandomString(128));
+
+            $connHand->insert("
+            INSERT INTO t_users(usename, usefirstname, usermail, usepassword, id_role, usetoken)
+            VALUES ('". $formCreator->getValue("nom") 
+            . "', '". $formCreator->getValue("prenom") 
+            . "', '". $formCreator->getValue("mail") 
+            . "', '". password_hash($formCreator->getValue("mdp"), PASSWORD_DEFAULT) . "', 3, '$token')
+            ");
+            
+            $from = "contact.news@gmail.com";
+            $to = $formCreator->getValue("mail");
+            $subject = "Vérification de votre compte";
+            $header = "Content-type: text/html; charset=iso-8859-1\nFrom:" . $from;
+            $message = "<a href='http://localhost/ExercicePHP/index.php?page=mailValidation&token=" 
+            . urlencode($token) . "&mail=" 
+                . urlencode(password_hash($formCreator->getValue("mail"), PASSWORD_DEFAULT)) . "' target='_blank'>Cliquez sur ce lien pour valider votre compte</a>";
+            mail($to, $subject, $message, $header);
+        }
+    } 
+
+    else 
+    {
+        echo $formCreator->checkErrors();
+        echo $form;
+    }
+
+} 
+
+else 
+{
+    echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
+    echo $form;
+}
+
