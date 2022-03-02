5dfe45ae141288448902d00141d1bcaffeecbb60
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 16:01:08 +0100

    Page rendu accessible seulement si connecté

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,5 +1,17 @@
 <?php
-$_SESSION['login'] = false;
-session_unset();
-session_destroy();
-echo "<script>document.location.replace('http://localhost/GEIPAN/');</script>";   
+
+if (!$_SESSION['login'])
+{
+    echo "<script>
+    document.location.replace('http://localhost/GEIPAN/index.php?page=404');
+    </script>";
+}
+
+else
+{
+    $_SESSION['login'] = false;
+    session_unset();
+    session_destroy();
+    echo "<script>document.location.replace('http://localhost/GEIPAN/');</script>";   
+
+}
