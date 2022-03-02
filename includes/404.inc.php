41f40cbeeb61d3b1a4618d7b87369dabf66507c6
Nouira Alim <nouiraalim@gmail.com>
2022-03-02 09:39:35 +0100

    Ajout d'une page 404

---
+++ includes/404.inc.php
@@ -0,0 +1,7 @@
+<div class="notFoundPage">
+    <h1>404</h1>
+
+    <img src="./assets/img/ufo.jpg" alt="Un ufo écrasé">
+
+    <h2>Oops, something went wrong with the page you are trying to access.</h2>
+</div>
